f2d52e1487aaa83b0dcd9f5a3bee03f17f505f7a
Guilherme Piaia <piaiag2@gmail.com>
2019-06-02 14:09:19 -0300

    CHANGED: created a function to write a file with the model, named Model.py

---
+++ Model.py
@@ -0,0 +1,3 @@
+import numpy as np 
+class model:    def __init__(self):        print('Init model')
+    def function(y, t):         Q = 2.0          d = 1.5          Omega = 0.65         theta, omega = y          derivs = [omega,                 -omega/Q + np.sin(theta) + d*np.cos(Omega*t)]        return derivs
